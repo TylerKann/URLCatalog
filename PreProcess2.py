51c0e3f80a246301c9d3bc085a2e756a6bacd538
TylerKann <43652037+TylerKann@users.noreply.github.com>
2021-11-24 11:49:50 -0500

    improved pandas DF

--- PreProcess2.py
+++ PreProcess2.py
@@ -11,18 +11,27 @@ file_name = "SiteList.csv"
 output_file = "FinalProcessed.csv"
 df = pd.read_csv(file_name, delimiter = ',')
 df.drop_duplicates(subset=None, inplace=True)
-
+''' 
+OLD ALPHANUM
 AlphaNum = {'a': 0, 'b': 0, 'c': 0, 'd':0, 'e':0, 'f':0, 'g': 0, 'h': 0, 'i':0, 'j':0, 'k':0, 'l': 0, 'm': 0,
 		    'n':0, 'o':0, 'p':0, 'q': 0, 'r': 0, 's':0, 't':0, 'u':0, 'v': 0, 'w': 0, 'x':0, 'y':0, 'z':0,
 		    '0': 0, '1': 0, '2':0, '3':0, '4':0, '5': 0, '6': 0, '7':0, '8':0, '9':0, '-': 0, '.': 0, '_':0,
 		     ':':0, '/':0, '?':0, '=':0, '&':0, '%':0, '#':0, '@':0, ';':0, '(':0, ')':0, '+':0, '\'':0, ',':0, '~':0, '*':0, '[':0, ']':0, '!':0, '$':0, ' ':0} # this is a list of all valid chars in a domain (base list)
+'''
+# This one is only valid domain chars, above one was all valid URL chars
+AlphaNum = {'a': 0, 'b': 0, 'c': 0, 'd':0, 'e':0, 'f':0, 'g': 0, 'h': 0, 'i':0, 'j':0, 'k':0, 'l': 0, 'm': 0,
+		    'n':0, 'o':0, 'p':0, 'q': 0, 'r': 0, 's':0, 't':0, 'u':0, 'v': 0, 'w': 0, 'x':0, 'y':0, 'z':0,
+		    '0': 0, '1': 0, '2':0, '3':0, '4':0, '5': 0, '6': 0, '7':0, '8':0, '9':0, '-': 0, '.': 0, '_':0} # this is a list of all valid chars in a domain (base list)
+
+
+
 
 urls = df.iloc[:,0] # grab all the URLS (0th column all rows)
 newData = []
 
-newDF = pd.DataFrame()
-certs = np.zeros((1, len(urls)))
-
+COLS = ['Num'] + ['Inception', 'Expiration', 'Updated', 'Name Servers', 'Auth Servers', 'SSL Cert'] + list(key for key in AlphaNum.keys()) + ['Site Type']
+print(COLS)
+newDF = pd.DataFrame(columns = COLS)
  
 
 def registered_and_date(domain_name):
@@ -82,9 +91,11 @@ def url_data(domain_name):
 	ANList = AlphaNum.copy() # make a copy of it for this specific url 
 	url = str.lower(domain_name) # upper case and lower case function the same for domain name 
 	for char in url: 
-		ANList[char] += 1 # increment the counter when we count a  char 
+		if char in ANList:
+			ANList[char] += 1 # increment the counter when we count a  char 
 	listForm = np.asarray(list(ANList.values())) # this will convert the dict into string (e.g. aaaa.com -> [4, 0, 1... ])
 	return listForm
+
 
 
 
@@ -95,7 +106,7 @@ for i in range(len(urls)):
 	if i % 100 == 0:
 		print(f"urls processed: {i}")
 		print(f"urls kept: {j}")
-	if i % 10000 == 0: 
+	if i % 20 == 0: 
 		newDF.to_csv(output_file, index = False) 
 		print("Backup Complete")
 	url = urls[i] 
@@ -108,11 +119,11 @@ for i in range(len(urls)):
 	cert =  getCert(url) # by this point we should hope that we have an active site 
 	urlData = url_data(url) # getting meta data of the url (character breakdown) 
 
-	finalOutput = np.concatenate(([urlData, dates, A, cert, [df.iloc[i,1]]])) # this adds on the Y value that we ultimately want to predict 
-	newDF = newDF.append([finalOutput.tolist()])
+	finalOutput = np.concatenate(([[i], dates, A, cert, urlData, [df.iloc[i,1]]])) # this adds on the Y value that we ultimately want to predict 
+	mergeDF = pd.DataFrame([finalOutput], columns = COLS)
+	#newDF = newDF.merge(mergeDF, how = 'outer')
+	newDF = newDF.append(mergeDF)
 	j += 1
 
-print("New Data Looks like") 
-print(finalOutput)
-dfNew = pd.DataFrame(newData)
-dfNew.to_csv(output_file, index=False)
+print(f"{j*100/len(urls)} % of URLS kept")
+newDF.to_csv(output_file, index=False)
